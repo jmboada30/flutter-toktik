de9edecf9acf7f3680beee0eb6c34abadfb3e42f
Joel M. Boada <jmboada30@gmail.com>
2024-02-18 11:01:11 -0400

    Add gradient backgrounds and text to VideoScrollableView

--- lib/presentation/widgets/shared/vide_scrollable_view.dart
+++ lib/presentation/widgets/shared/vide_scrollable_view.dart
@@ -17,14 +17,31 @@ class VideoScrollableView extends StatelessWidget {
                   colors: [Colors.yellow, Colors.orange],
                   begin: Alignment.topLeft,
                   end: Alignment.bottomRight)),
+          child: const Center(
+            child: Text(
+              'Gradiente Vertical',
+              style: TextStyle(fontSize: 24, color: Colors.black),
+            ),
+          ),
         ),
         Container(
           decoration: const BoxDecoration(
-              gradient:
-                  LinearGradient(colors: [Colors.blue, Colors.purple])),
+              gradient: LinearGradient(colors: [Colors.blue, Colors.purple])),
+          child: const Center(
+            child: Text(
+              'Gradiente Horizontal',
+              style: TextStyle(fontSize: 24, color: Colors.black),
+            ),
+          ),
         ),
         Container(
           color: Colors.greenAccent,
+          child: const Center(
+            child: Text(
+              'Color Solido',
+              style: TextStyle(fontSize: 24, color: Colors.black),
+            ),
+          ),
         )
       ],
     );
